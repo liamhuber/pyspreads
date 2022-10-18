ea27402579c4671c239e2d34cd09f7fee8aa49dc
Liam Huber <liam.huber@gmail.com>
2022-10-17 21:40:58 -0700

    Update on position changes

--- pyspreads/gui/gui.py
+++ pyspreads/gui/gui.py
@@ -11,44 +11,76 @@ class VerticalGUI(VerticalModel):
         self.market_gui = SASEMarketGUI(self)
         self.market_display = widgets.Output()
         self.portfolio_display = widgets.Output()
-
-    def draw(self):
-        self.market_gui.draw()
-        self.market_display.clear_output()
-        with self.market_display:
-            display(self.plot_market()[0])
-        self.portfolio_display.clear_output()
-        with self.portfolio_display:
-            display(self.plot_value()[0])
-
-        portfolio_summary = widgets.VBox(
+        self.portfolio_summary = widgets.VBox(
             [
                 widgets.Label(f"MAX RETURN = {self.max_return}"),
                 widgets.Label(f"EXPECTATION = {self.expectation}"),
                 widgets.Label(f"MAX DRAWDOWN = {self.max_drawdown}"),
             ]
         )
-
-        main = widgets.HBox(
+        self.trade_screen = widgets.HBox(
             [
                 self.market_gui.widget,
                 self.portfolio_display,
                 widgets.VBox(
-                    [self.market_display, portfolio_summary],
+                    [self.market_display, self.portfolio_summary],
                     layout=widgets.Layout(width='33%')
                 )
             ]
         )
-        tabs = widgets.Tab(
+
+        self.tabs = widgets.Tab(
             [
-                main,
+                self.trade_screen,
                 self.market_display,
-                widgets.HBox([self.portfolio_display, portfolio_summary])
+                widgets.HBox([self.portfolio_display, self.portfolio_summary])
             ],
             layout=widgets.Layout(height='500px')
         )
-        tabs.set_title(0, 'Trade')
-        tabs.set_title(1, 'Market')
-        tabs.set_title(2, 'Portfolio')
+        self.tabs.set_title(0, 'Trade')
+        self.tabs.set_title(1, 'Market')
+        self.tabs.set_title(2, 'Portfolio')
 
-        return tabs
+    def draw_market_plot(self):
+        self.market_display.clear_output()
+        with self.market_display:
+            display(self.plot_market()[0])
+
+    def draw_portfolio_plot(self):
+        self.portfolio_display.clear_output()
+        with self.portfolio_display:
+            display(self.plot_value()[0])
+
+    def update_portfolio_summary(self):
+        self.portfolio_summary.children[0].value = f"MAX RETURN = {self.max_return}"
+        self.portfolio_summary.children[1].value = f"EXPECTATION = {self.expectation}"
+        self.portfolio_summary.children[2].value = f"MAX DRAWDOWN = {self.max_drawdown}"
+
+    def update_market_gui(self):
+        """Careful, this one is expensive"""
+        self.market_gui = SASEMarketGUI(self)
+        self.market_gui.draw()
+
+    def update_market(self):
+        self.update_market_gui()
+        self.draw_market_plot()
+
+    def update_portfolio(self):
+        self.draw_portfolio_plot()
+        self.update_portfolio_summary()
+
+    def update(self):
+        self.update_market()
+        self.update_portfolio()
+
+    def draw(self):
+        self.update()
+        return self.tabs
+
+    def take_position(self, price: float, strike: float, long_or_short: str, call_or_put: str):
+        super().take_position(price=price, strike=strike, long_or_short=long_or_short, call_or_put=call_or_put)
+        self.update_portfolio()
+
+    def remove_position(self, strike: float, long_or_short: str, call_or_put: str):
+        super().remove_position(strike=strike, long_or_short=long_or_short, call_or_put=call_or_put)
+        self.update_portfolio()
